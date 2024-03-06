bc7420d7ffe04f9a9a562d5b0b100011c631e4d1
Gadisa Tesfu Gonfa <gadisatesfug@siinqeebank.com>
2024-03-06 14:26:34 +0300

    s/thing is edited

--- src/App.js
+++ src/App.js
@@ -7,15 +7,9 @@ import Footer from "./components/Footer/Footer";
 import { SpeedInsights } from "@vercel/speed-insights/react"
             
 function App() {
-  const appStyles = {
-    backgroundColor: 'rgb(7, 7, 7)',
-    color: 'whitesmoke',
-    overflow: 'hidden',
-    overflowY: 'scroll'
-  };
-
+  
   return (
-    <div className="App" style={appStyles}>
+    <div className="App">
       
       <NavBar />
       
